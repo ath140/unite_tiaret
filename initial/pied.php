e3406cc2a641fda9020c89c534e93c9423ab2bb0
ath140 <athmohamed140gmail.com>
2025-01-14 11:14:44 +0100

    gds

---
+++ initial/pied.php
@@ -0,0 +1,32 @@
+ <!-- Control Sidebar -->
+ <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../adminlte-v3/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../adminlte-v3plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../adminlte-v3dist/js/adminlte.min.js"></script>
+</body>
+</html>
